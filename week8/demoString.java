9ae0543163202868d4f029ea2e699e693397be88
RoshanAli339 <roshanali339lps@gmail.com>
2022-12-29 19:50:01 +0530

    Thursday 29 December 2022 07:50:01 PM IST

--- week8/demoString.java
+++ week8/demoString.java
@@ -4,7 +4,7 @@ class demoString
 {
 	public static void main(String args[])
 	{
-		String str = "    RVR & JC College of Engineering";
+		String str = "    RVR & JC College"; 
 		System.out.println("Original string: " + str);
 		System.out.println("trim(): " + str.trim());
 		str = str.trim();
@@ -15,7 +15,7 @@ class demoString
 		System.out.println("indexOf('o'): " + str.indexOf('o'));
 		System.out.println("substring(3, 10): " + str.substring(3, 10));
 		System.out.println("replace(\"RVR & JC\", \"R.V.R & J.C\"): " + str.replace("RVR & JC", "R.V.R & J.C"));
-		System.out.println("equalsIgnoreCase(\"rvr & jc cOLLEGE OF eNGINEERING\"): " + str.equalsIgnoreCase("rvr & jc cOLLEGE OF eNGINEERING"));
-		System.out.println("contains(\"of\"): " + str.contains("of"));
+		System.out.println("equalsIgnoreCase(\"rvr & jc cOLLEGe\"): " + str.equalsIgnoreCase("rvr & jc cOLLEGe"));
+		System.out.println("contains(\"&\"): " + str.contains("&"));
 	}
 }
--- week8/demoStringBuffer.java
+++ week8/demoStringBuffer.java
@@ -4,17 +4,20 @@ class demoStringBuffer
 {
 	public static void main(String args[])
 	{
-		StringBuffer str = new StringBuffer("	RVR & JC COLLEGE OF ENGINEERING");
+		StringBuffer str = new StringBuffer("RVR & JC COLLEGE");
+        StringBuffer str2 = new StringBuffer();
 		System.out.println("Original string buffer: " + str);
-		System.out.println("append9(5.34): " + str.append(5.34));
-		System.out.println("length(): " + str.length());
-		System.out.println("reverse(): " + str.reverse());
-		System.out.println("capacity(): " + str.capacity());
-		System.out.println("charAt(5): " + str.charAt(5));
-		System.out.println("codePointAt(5): " + str.codePointAt(5));
-		System.out.println("codePointBefore(5): " + str.codePointBefore(5));
-		System.out.println("delete(3, 6): " + str.delete(3, 6));
-		System.out.println("delteCharAt(11): " + str.deleteCharAt(11));
-		System.out.println("indexOf(\"J\"): " + str.indexOf("J"));
+        System.out.println("str.capacity(): " + str.capacity());
+        System.out.println("str.length(): " + str.length());
+        System.out.println("Second string buffer: " + str2);
+        System.out.println("str2.capacity(): " + str2.capacity());
+		System.out.println("str.append9(5.34): " + str.append(5.34));
+		System.out.println("str.reverse(): " + str.reverse());
+		System.out.println("str.charAt(5): " + str.charAt(5));
+		System.out.println("str.codePointAt(5): " + str.codePointAt(5));
+		System.out.println("str.codePointBefore(5): " + str.codePointBefore(5));
+		System.out.println("str.delete(3, 6): " + str.delete(3, 6));
+		System.out.println("str.delteCharAt(11): " + str.deleteCharAt(11));
+		System.out.println("str.indexOf(\"J\"): " + str.indexOf("J"));
 	}	
 }
